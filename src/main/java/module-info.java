e44a6b71571fff9817a1b601e9631777ad2c06ea
Naman22311 <119219722+Naman22311@users.noreply.github.com>
2023-12-10 05:46:16 +0530

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.stickman {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.ap.stickman to javafx.fxml;
-    exports com.ap.stickman;
-}
